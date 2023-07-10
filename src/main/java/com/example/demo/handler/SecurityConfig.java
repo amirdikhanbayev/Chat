6fb2113d097b343fc4902534da40336ee67e49f9
Amir <amir.dikhanbayev@gmail.com>
2023-07-10 17:50:58 +0600

    Security not All Token left

---
+++ src/main/java/com/example/demo/handler/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.example.demo.handler;
+
+import com.example.demo.service.user.UserDetailServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Autowired
+    private CustomAuthenticationFailureHandler customAuthenticationFailureHandler;
+    @Autowired
+    private CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+    @Autowired
+    private UserDetailServiceImpl userDetailService;
+
+    private void sharedSecurityConfiguration(HttpSecurity http) throws Exception{
+        http.csrf(AbstractHttpConfigurer::disable);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        sharedSecurityConfiguration(http);
+        http.authorizeRequests()
+                .requestMatchers("/login*").permitAll()
+                .requestMatchers("/user*").permitAll()
+                .anyRequest().authenticated().and()
+                .formLogin(from ->
+                        from.loginProcessingUrl("/login")
+                                .usernameParameter("login")
+                                .passwordParameter("password")
+                                .failureHandler(customAuthenticationFailureHandler)
+                                .successHandler(customAuthenticationSuccessHandler));
+        return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception{
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailService);
+        authenticationManagerBuilder.authenticationProvider(authenticationProvider());
+        return authenticationManagerBuilder.build();
+    }
+}
